e1e611ebefd9ee195d50012f3ac1ca65f3401de9
nctrung10 <chitrunghg111@gmail.com>
2022-06-20 17:06:38 +0700

    Install mongoDB, mongoose; Create models

--- src/index.js
+++ src/index.js
@@ -4,8 +4,14 @@ const handlebars = require('express-handlebars');
 const morgan = require('morgan');
 const app = express();
 const port = 3000;
-const route = require('./routes');
 
+const route = require('./routes');
+const db = require('./config/db');
+
+// Connect to database
+db.connect();
+
+// Use extensions
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -16,12 +22,12 @@ app.use(express.json());
 // Template engine
 app.engine('hbs', handlebars.engine({ extname: '.hbs' }));
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources/views'));
+app.set('views', path.join(__dirname, 'resources', 'views'));
 
 // Routes init
 route(app);
 
 //
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+    console.log(`App listening on port ${port}`);
 });
